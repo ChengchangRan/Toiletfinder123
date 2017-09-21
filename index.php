318432c4d178c3bdfebdb52df437e84b634e2bb7
kingsleyren <31723907+kingsleyren@users.noreply.github.com>
2017-09-21 15:46:41 +1000

    database connection file

--- index.php
+++ index.php
@@ -1,8 +1,56 @@
+<!DOCTYPE html>
+<!-- database using XML file -->
 <?php
+
+require("phpsqlajax_dbinfo.php");
+
+// Start XML file, create parent node
+
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+// Opens a connection to a MySQL server
+
+$connection=mysql_connect ('localhost', $username, $password);
+if (!$connection) {  die('Not connected : ' . mysql_error());}
+
+// Set the active MySQL database
+
+$db_selected = mysql_select_db($database, $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysql_error());
+}
+
+// Select all the rows in the markers table
+
+$query = "SELECT * FROM markers WHERE 1";
+$result = mysql_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+
+while ($row = @mysql_fetch_assoc($result)){
+  // Add to XML document node
+  $node = $dom->createElement("marker");
+  $newnode = $parnode->appendChild($node);
+  $newnode->setAttribute("type",$row['type']);
+  $newnode->setAttribute("gender", $row['gender']);
+  $newnode->setAttribute("baby", $row['baby']);
+  $newnode->setAttribute("disable", $row['disable']);
+  $newnode->setAttribute("lat", $row['lat']);
+  $newnode->setAttribute("lng", $row['lng']);
+  $newnode->setAttribute("description", $row['description']);
+}
+
+echo $dom->saveXML();
 
 ?>
 
-<!DOCTYPE html>
 <html>
 
   <header>
